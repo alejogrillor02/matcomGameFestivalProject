ff3a04da3233f7cef26527ce07227fbb30489b0c
AlejandroSicilia02 <elsicijr@gmail.com>
2025-04-27 19:05:01 -0400

    Exit

--- scenes/scripts/game.gd
+++ scenes/scripts/game.gd
@@ -8,6 +8,7 @@ extends Node2D
 @onready var press_space_text = $CanvasLayer/PressSpaceText
 @onready var restart_button = $ButtonLayer/Button
 
+@onready var exit_instance = $exit # Instancia de la salida
 
 var dirs = {
 	"N": Vector2i(0, 1),
@@ -19,6 +20,8 @@ var dirs = {
 var dwarf_pos: Vector2i
 var dwarf_dir = "E"
 
+var exit_pos: Vector2i
+
 var grid: Array = []
 var game_started = false
 
@@ -26,6 +29,9 @@ var game_started = false
 func _ready() -> void:
 	# Determinar posición inicial basada en la posición del nodo
 	dwarf_pos = local_to_used_rect(dwarf_instance.position + Vector2(13, 13))
+	# Posición de la salida
+	exit_pos = local_to_used_rect(exit_instance.position + Vector2(13, 13))
+	
 	
 	# Preparar el Grid
 	for y in tile_map.get_used_rect().size.y:
@@ -43,6 +49,7 @@ func _ready() -> void:
 			else:
 				grid[y].append(1)
 				
+	#Empezar con el boton deshabilitado
 	restart_button.disabled = true
 
 
@@ -56,7 +63,9 @@ func _process(_delta):
 		var tween = create_tween()
 		tween.tween_property(camera, "zoom", Vector2(2, 2), 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
 		
+		#Habilitar el boton de reinicio
 		restart_button.disabled = false
+		
 
 
 func local_to_used_rect(pixel_pos: Vector2) -> Vector2i:
@@ -108,25 +117,29 @@ func _on_move_timer_timeout() -> void:
 	else:
 		dwarf_dir = valid_dirs[0]
 		
-	move_dwarf(dwarf_dir)
-	$MoveTimer.start()
+	if(dwarf_pos!=exit_pos):
+		move_dwarf(dwarf_dir)
+		$MoveTimer.start()
+	else:
+		await get_tree().create_timer(1).timeout
+		get_tree().reload_current_scene()
 
 
 func move_dwarf(direction: String):
 	dwarf_pos += dirs[direction]
 	
-	var target_position = used_rect_to_local(dwarf_pos) - Vector2(13, 13)
-	
+	var target_position = used_rect_to_local(dwarf_pos) - Vector2(13, 13)	
 	# Mover el enano suavemente
 	var tween = create_tween()
 	tween.tween_property(dwarf_instance, "position", target_position, 0.2)
-	
-	# Configurar flip horizontal según dirección
+
+# Configurar flip horizontal según dirección
 	var animated_sprite = dwarf_instance.get_node("AnimatedSprite2D")
 	if direction == "E":
 		animated_sprite.flip_h = false
 	elif direction == "W":
 		animated_sprite.flip_h = true
-	
+
 	if not animated_sprite.is_playing():
 		animated_sprite.play("walk")
+
